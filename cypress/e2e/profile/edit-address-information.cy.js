30b68f890083cafe56e2a4b4b0be941326bf4792
suroso <suroso@suroso.local>
2024-02-05 20:01:43 +0700

    [S] add edit address

--- cypress/e2e/profile/edit-address-information.cy.js
+++ cypress/e2e/profile/edit-address-information.cy.js
@@ -14,16 +14,25 @@ describe('Edit Account Information', () => {
       cy.get('#send2').click();
     });
 
-    it('Edit Account', () => {
+    it('Edit Address', () => {
       // Ambil nilai email dari variabel Cypress.env
       const registeredEmail = Cypress.env('registeredEmail');
       cy.visit('https://magento.softwaretestingboard.com/');
       cy.contains('Sign In').click();
       cy.url().should('eq', 'https://magento.softwaretestingboard.com/customer/account/');
-      cy.get('#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-content > div > div.box-actions > a.action.edit > span').click();
+      cy.get('#maincontent > div.columns > div.column.main > div.block.block-dashboard-addresses > div.block-title > a > span').click();
       cy.get('#firstname').type('User7')
       cy.get('#lastname').type('User7')
-      cy.get('#form-validate > div > div.primary > button').click();
+      cy.get('#company').type('PT.ABC')
+      cy.get('#telephone').type('628456565463654')
+      cy.get('#street_1').type('Bandung')
+      cy.get('#street_2').type('Cimahi')
+      cy.get('#street_3').type('CImahi Utara')
+      cy.get('#city').type('Bandung')
+      cy.get('#region_id').type('Bandung')
+      cy.get('#zip').type('956467')
+      cy.get('#country').type('Indonesia')
+      cy.get('#form-validate > div > div.primary > button > span').click();
     });
 
 
